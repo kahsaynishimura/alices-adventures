8fa33d4fcce04e366f5ab9a35cc1512a95a6deec
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2015-12-19 21:17:03 -0200

    versão 1.1 - alice word by word
    Algumas palavras que estavam com separação de silabas foram corrigidas.
    A imagem inicial foi trocada
    O aplicativo nao pede que logue, so mostra uma splash screen

--- AlicesAdventuresInWonderland/app/src/main/java/com/karina/alicesadventures/SelectUserActivity.java
+++ AlicesAdventuresInWonderland/app/src/main/java/com/karina/alicesadventures/SelectUserActivity.java
@@ -3,6 +3,7 @@ package com.karina.alicesadventures;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -18,48 +19,57 @@ import java.util.Date;
 
 public class SelectUserActivity extends ActionBarActivity {
 
+    private static int SPLASH_TIME_OUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
-    }
+        new Handler().postDelayed(new Runnable() {
 
-    public User findUser(View v) {
-        DBHandler db = null;
-        User user = new User();
-        try {
-            InputStream is = getBaseContext().getAssets()
-                    .open(DBHandler.DATABASE_NAME);
-            db = new DBHandler(this, is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (db != null) {
-            String code = ((EditText) findViewById(R.id.student_code)).getText().toString();
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
 
-            user = db.findUser(code);
-            if (user != null) {
-                Intent i = new Intent(SelectUserActivity.this, LessonActivity.class);
+            @Override
+            public void run() {
+                DBHandler db = null;
+                User user = new User();
+                try {
+                    InputStream is = getBaseContext().getAssets()
+                            .open(DBHandler.DATABASE_NAME);
+                    db = new DBHandler(SelectUserActivity.this, is);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (db != null) {
+                   // String code = ((EditText) findViewById(R.id.student_code)).getText().toString();
 
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(SelectUserActivity.this);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
+                    //user = db.findUser(code);
+                    //if (user != null) {
+                        Intent i = new Intent(SelectUserActivity.this, LessonActivity.class);
 
-                //getting the current time in milliseconds, and creating a Date object from it:
-                Date date = new Date(System.currentTimeMillis()); //or simply new Date();
+                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(SelectUserActivity.this);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
 
-                //converting it back to a milliseconds representation:
-                long millis = date.getTime();
+                        //getting the current time in milliseconds, and creating a Date object from it:
+                        Date date = new Date(System.currentTimeMillis()); //or simply new Date();
 
-                editor.putInt("exercise_count", 0);
-                editor.putInt("correct_sentence_count", 0);
-                editor.putInt("wrong_sentence_count", 0);
-                editor.putInt("user_id", user.get_id());
-                editor.putInt("last_lesson_completed_id", user.getLastCompletedLessonId());
-                editor.commit();
-                startActivity(i);
-                finish();
+                        //converting it back to a milliseconds representation:
+                        long millis = date.getTime();
+
+                        editor.putInt("exercise_count", 0);
+                        editor.putInt("correct_sentence_count", 0);
+                        editor.putInt("wrong_sentence_count", 0);
+                       // editor.putInt("user_id", user.get_id());
+                      //  editor.putInt("last_lesson_completed_id", user.getLastCompletedLessonId());
+                        editor.commit();
+                        startActivity(i);
+                        finish();
+                 //   }
+                }
             }
-        }
-        return user;
+        }, SPLASH_TIME_OUT);
     }
 }
